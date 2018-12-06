5f5a8106c2b7dc62fc8a0499511f91649a091533
crissy25 <crystal13dsouza@gmail.com>
2018-12-06 19:49:10 +0530

    build folder

---
+++ build/precache-manifest.99c4f5373250b501e38252f8484df2f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e56728639f9c5bd443c0",
+    "url": "/static/js/main.e5672863.chunk.js"
+  },
+  {
+    "revision": "aa278d1ff55d4af5832c",
+    "url": "/static/js/1.aa278d1f.chunk.js"
+  },
+  {
+    "revision": "e56728639f9c5bd443c0",
+    "url": "/static/css/main.0211efbb.chunk.css"
+  },
+  {
+    "revision": "8125ce5bd15a05878015f707dbea73c9",
+    "url": "/index.html"
+  }
+];
